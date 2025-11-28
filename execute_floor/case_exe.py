c8f2dde8cc1c661a4dbc61d8320dd90822ed7349
17538254325 <a17538254325@163.com>
2025-11-28 11:47:25 +0800

    Delete execute_floor directory

--- execute_floor/case_exe.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import os
-
-pytest.main(["../case_floor/appium_yemian.py","-vs","--alluredir","../report_floor/data"])
-
-os.system("allure generate ../report_floor/data -o ../report_floor/html --clean")
